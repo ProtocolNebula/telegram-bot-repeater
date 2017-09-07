629bf4e4e62a00be2a2e41ff37c2cba240982ea8
ProtocolNebula <powergm@hotmail.es>
2017-09-08 00:37:05 +0200

    Admin support

--- src/index.js
+++ src/index.js
@@ -13,34 +13,74 @@ app.telegram.getMe().then((botInfo) => {
   console.log("Bot username: @" + botInfo.username)
 })
 
+/**
+ * Check if the current user is admin
+ * @param {type} ctx
+ * @returns {Promise}
+ */
+function isAdmin(ctx) {
+    return new Promise(function(resolve, reject) {
+        ctx.getChat().then(function(chat) {
+
+            // Check if all users are admin
+            if (chat.all_members_are_administrators) {
+                resolve()
+                return
+            }
+
+            // Check if user is admin
+            ctx.getChatAdministrators().then(function(admins) {
+                const currentUser = ctx.update.message.from.id
+
+                for (let key in admins) {
+                    if (admins[key].user.id == currentUser) {
+                        resolve()
+                        return
+                    }
+                }
+                
+                reject()
+            })
+
+        })
+    })
+    
+}
+
 app.use(commandParts());
 app.use((ctx, next) => {
-    //console.log(ctx.update.message.from);
-    if (ctx.update.message.from.id != "13061540") {
-        return;
+    try {
+        if (ctx) {
+            isAdmin(ctx).then((res) => {
+                console.log(ctx.contextState)
+                if ('command' in ctx.contextState) {
+                    switch (ctx.contextState.command.command) {
+                        case "stop":
+                            stop(ctx)
+                            break;
+                        case "set":
+                            setMessage(ctx)
+                            break;
+                    }
+                }
+            }).catch((error) => {
+                ctx.reply("Only admins can set the message")
+            })
+            
+            
+        } 
+    } catch (ex) {
+        console.log(ex)
     }
-    //console.log(ctx.update.message.from)
-    //console.log(ctx.update.message.chat)
-    if ('command' in ctx.contextState) {
-        switch (ctx.contextState.command.command) {
-            case "stop":
-                stop(ctx)
-                break;
-            case "set":
-                setMessage(ctx)
-                break;
-        }
-    }
-    //console.log(ctx)
     
     return next()
 });
   
 app.command('start', (ctx) => {
     ctx.reply('Add this bot to your group and make a repeat string with:\n' +
-    '/set@GroupRepeatBot SECONDS MESSAGE TO SEND\n\n'+
+    '/set@'+app.options.username+' SECONDS MESSAGE TO SEND\n\n'+
     'EXAMPLE:\n'+
-    '/set@GroupRepeatBot 30 You must remember who\'s the admin\n\n'+
+    '/'+app.options.username+' 30 You must remember who\'s the admin\n\n'+
     'If you need to stop send:\n'+
     '/stop@GroupRepeatBot'
     )
@@ -53,7 +93,6 @@ function stop(ctx) {
         const chat = ctx.update.message.chat
         delete ActiveChats[chat.id]
         console.log("Message stopped from chat ", chat.id)
-        
     } catch (ex) {
         console.log(ex)
     }
@@ -64,11 +103,11 @@ function setMessage(ctx) {
         const command = ctx.contextState.command
         const seconds = command.splitArgs[0]
         delete command.splitArgs[0]
-        const message = command.splitArgs.slice(1).join(" ")
+        const message = command.splitArgs.slice(1).join(' ')
         
         if (seconds < config.MIN_SECONDS_BETWEEN_SENDS - 1) {
-            ctx.reply('The minium time between message is ' + config.MIN_SECONDS_BETWEEN_SENDS + ' seconds')
-        } else if (message == "") {
+            ctx.reply('The minimum time between message is ' + config.MIN_SECONDS_BETWEEN_SENDS + ' seconds')
+        } else if (message == '') {
             ctx.reply('You must specify a message')
         } else {
             ctx.getChat().then((info) => {
@@ -82,15 +121,7 @@ function setMessage(ctx) {
 
                 console.log("New message added: " ,message)
             })
-            
         }
-        
-        //console.log(command.splitArgs[0])
-        //if (command.splitArgs[1])
-        //console.log(command)
-
-        
-        
     } catch (ex) {
         console.log('Error adding message ' , ex)
     }
@@ -100,13 +131,13 @@ app.startPolling()
 
 setInterval(function() {
     const currentTime = Math.floor(Date.now())
-    try {
-        for (let chat in ActiveChats) {
-            //console.log(chat)
-            ActiveChats[chat].sendMessage(currentTime)
+    for (let chat in ActiveChats) {
+        console.log("Enviando a " + chat)
+        try {
+            ActiveChats[chat].sendMessage(currentTime)   
         }
-        
-    } catch (ex) {
-        console.log(ex)
+        catch (exception) {
+            console.log(exception)
+        }
     }
-}, config.SECONDS_CYCLE)
+}, config.SECONDS_CYCLE * 1000)
